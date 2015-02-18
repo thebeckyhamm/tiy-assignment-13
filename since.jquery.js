5b8003fb3f513b1556faaa9da2e6d3036cc920c2
Becky Hamm <becky@metricslice.com>
2015-02-18 16:36:31 -0500

    added since.jquery.js got it to work except setInterval

--- since.jquery.js
+++ since.jquery.js
@@ -5,15 +5,40 @@
   $.fn.since = function() {
 
     // Iterate over each node that was selected
+
     this.each(function() {
+      var $this = $(this);
+      var $date = moment($this.data("since")) || moment(); 
+      var now = moment();
+      var $span = $("<span>" + "</span>");
+      $this.append($span);
 
-      // in each iteration `this` is going to be
-      // the raw dom node so we need to wrap jQuery
-      // around it to do something with it.
+      if( now.diff($date, "seconds") < (60000)) {
+        $span.text("just now");
+      }
 
-      $(this).css({
-        "color"  : "red",
-        "border" : "1px solid red"
+      else if (now.diff($date, "minutes") < 5) {
+        $span.text("a few minutes ago");
+      }
+
+      else if (now.diff($date, "hour") < 1) {
+        $span.text("within the past hour");
+      }
+
+      else if (now.diff($date, "day") < 1) {
+        $span.text("today");
+      }
+
+      else if (now.diff($date, "day") < 6) {
+        $span.text($date.format("dddd"));
+      }
+
+      else if (now.diff($date, "day") >= 6) {
+        $span.text($date.format("MM-DD-YYYY"));
+      }
+
+      $span.css({
+        "margin-left"  : "10px"
       });
 
     });
@@ -21,6 +46,9 @@
     // Return `this` to make this plugin chainable
     return this;
 
+
   };
+
+
 
 })(jQuery);
